15f3ecb7d30e23a3f7b171b79a6332debc3aa9be
Dale Bendixen <dalebendixen@gmail.com>
2024-06-17 16:13:21 -0400

    added main page routing and centered home page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,42 @@
 
 
 <div class="header">
-    <p>
-        This site is designed for <em>Rideshare drivers </em>to share their increasingly <em>low</em> pay offered by the largest Rideshare companies.
-    </p>
-    <p>
-        The <em>information</em> and screenshots on this site are meant to <em>inform</em> the general public, and legislators to <em>affect change!</em>
-    </p>
+    <div>
+        <p>
+            This site is designed for <em>Rideshare drivers </em>to share their increasingly <em>low</em> pay offered by the largest Rideshare companies.
+        </p>
+        <p>
+            The <em>information</em> and screenshots on this site are meant to <em>inform</em> the general public, and legislators to <em>affect change!</em>
+        </p>
+        <a href="/main">
+            <em><h1>Enter Site</h1></em>
+        </a>
+
+    </div>
 </div>
 
 
 <style>
     .header {
-        border: 1px solid red;
+        display: grid;
+        grid-template-rows: 1fr;
         width: 90dvw;
-        margin: 10em  auto 0 auto;
+        height: 100dvh;
+        margin: auto;
+        align-items: center;
         text-align: center;
     }
     em {
         color: rgb(249, 123, 123);
+    }
+    a {
+        background: none;
+        border: none;
+        cursor: pointer;
+        text-decoration: none;
+    }
+    a em h1:hover {
+        color: rgb(250, 202, 202);
     }
 
 </style>
