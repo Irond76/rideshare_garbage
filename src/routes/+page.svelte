7a2b229949f5c4dcf6292b66e09bb865c1eaa575
Dale Bendixen <dalebendixen@gmail.com>
2024-06-17 15:26:37 -0400

    added info to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<div class="header">
+    <p>
+        This site is designed for <em>Rideshare drivers </em>to share their increasingly <em>low</em> pay offered by the largest Rideshare companies.
+    </p>
+    <p>
+        The <em>information</em> and screenshots on this site are meant to <em>inform</em> the general public, and legislators to <em>affect change!</em>
+    </p>
+</div>
+
+
+<style>
+    .header {
+        border: 1px solid red;
+        width: 90dvw;
+        margin: 10em  auto 0 auto;
+        text-align: center;
+    }
+    em {
+        color: rgb(249, 123, 123);
+    }
+
+</style>
